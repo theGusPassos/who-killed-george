d5537b92ea03bd185a7b1b6491da8130aa8038b3
theguspassos <theguspassos@gmail.com>
2021-06-12 02:45:42 -0300

    basic cutscene

--- Assets/Scripts/Cutscene/InterrogationStarter.cs
+++ Assets/Scripts/Cutscene/InterrogationStarter.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.Scripts.Cutscene.Data;
+using Assets.Scripts.UI.Text;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,10 +12,15 @@ namespace Assets.Scripts.Cutscene
         public static InterrogationStarter Instance;
         CanvasGroup canvasGroup;
 
+        [SerializeField] float timeToBackgroundFade;
         [SerializeField] float timeToFade;
         float timer;
 
         [SerializeField] Image characterFace;
+        [SerializeField] float timeToShowCharacterName;
+        [SerializeField] TextMeshProUGUI characterNameMesh;
+        [SerializeField] float timeToStartTyping;
+        [SerializeField] TextTyper textTyper;
 
         private void Awake()
         {
@@ -30,7 +37,7 @@ namespace Assets.Scripts.Cutscene
         public void StartInterrogation(CharacterData characterData, InterrogationData interrogationData)
         {
             StartCoroutine(FadeIn());
-            StartCoroutine(ShowCharactersFace());
+            StartCoroutine(ShowCharactersFace(characterData.sprite, characterData.name));
         }
 
         IEnumerator FadeIn()
@@ -38,20 +45,35 @@ namespace Assets.Scripts.Cutscene
             while (canvasGroup.alpha < 1)
             {
                 timer += Time.deltaTime;
-                canvasGroup.alpha = Mathf.Lerp(0, 1, timer / timeToFade);
+                canvasGroup.alpha = Mathf.Lerp(0, 1, timer / timeToBackgroundFade);
                 yield return new WaitForEndOfFrame();
             }
         }
 
-        IEnumerator ShowCharactersFace()
+        IEnumerator ShowCharactersFace(Sprite sprite, string name)
         {
+            characterFace.sprite = sprite;
+            characterNameMesh.text = name;
+
             yield return new WaitForSeconds(timeToFade + 0.5f);
+
+            timer = 0;
 
             while (characterFace.color.a < 1)
             {
-            characterFace.color += new Color(0, 0, 0, Time.deltaTime);
+                timer += Time.deltaTime;
+                var curr = Mathf.Lerp(0, 1, timer / timeToFade);
+                characterFace.color = new Color(characterFace.color.r, characterFace.color.g, characterFace.color.b, curr);
+                characterNameMesh.color = new Color(255, 255, 255, curr);
+
                 yield return new WaitForEndOfFrame();
             }
+        }
+
+        IEnumerator TypeTextTyper()
+        {
+            yield return new WaitForSeconds(timeToStartTyping);
+
         }
     }
 }
---
+++ Assets/Scripts/Ui/Text/TextTyper.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Text
+{
+    public class TextTyper : MonoBehaviour
+    {
+        private TextMeshProUGUI textMesh;
+        private int currentLetter;
+        private string toType;
+        [SerializeField] float timeBetweenWords;
+        private float currentTimer;
+
+        private void Awake()
+        {
+            textMesh = GetComponent<TextMeshProUGUI>();
+        }
+
+        public void Type(string toType) => this.toType = toType;
+
+        private void Update()
+        {
+            if (string.IsNullOrEmpty(toType) || currentLetter == toType.Length)
+                return;
+
+            currentTimer += Time.deltaTime;
+
+            if (currentTimer > timeBetweenWords)
+            {
+                textMesh.text += toType[currentLetter];
+
+                // doesnt delay if is space
+                if (!string.IsNullOrWhiteSpace(toType[currentLetter].ToString()))
+                    currentTimer = 0;
+
+                currentLetter++;
+            }
+        }
+    }
+}
