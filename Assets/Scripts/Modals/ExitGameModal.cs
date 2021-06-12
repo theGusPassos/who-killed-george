fb1a68aae3f695a71d4d1f43a81c8c57f5ee6309
theguspassos <theguspassos@gmail.com>
2021-06-12 12:44:49 -0300

    add modal to quit game

---
+++ Assets/Scripts/Modals/ExitGameModal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Modals
+{
+    public class ExitGameModal : MonoBehaviour
+    {
+        public GameObject modal;
+
+        private void Update()
+        {
+            if (!modal.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+            {
+                modal.SetActive(true);
+            }
+            else if (modal.activeSelf && Input.anyKeyDown)
+            {
+                modal.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Options/CloseApplicationOption.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Options
+{
+    public class CloseApplicationOption : MonoBehaviour, IPointerDownHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Application.Quit();
+        }
+    }
+}
