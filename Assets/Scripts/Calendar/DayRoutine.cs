58543c8b157ecf2b6ee4a4250317b266ebb18916
theguspassos <theguspassos@gmail.com>
2021-06-13 13:54:20 -0300

    add abstain final

--- Assets/Scripts/Calendar/DayRoutine.cs
+++ Assets/Scripts/Calendar/DayRoutine.cs
@@ -1,4 +1,6 @@
-﻿using Assets.Scripts.Sound;
+﻿using Assets.Scripts.Cutscene.Data;
+using Assets.Scripts.Cutscene.Endgame;
+using Assets.Scripts.Sound;
 using TMPro;
 using UnityEngine;
 
@@ -21,18 +23,36 @@ namespace Assets.Scripts.Calendar
             SetRemainingDays(placesToMark.Length - currentMarker - 1);
         }
 
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+                MarkNext();
+        }
+
         public void MarkNext()
         {
             if (currentMarker == placesToMark.Length - 1)
             {
                 buttonToGiveUp.SetActive(true);
+                Instantiate(marker, placesToMark[currentMarker].transform.position, Quaternion.identity, transform);
             }
 
-            Djzada.Instance.CountNextDay();
-            SetRemainingDays(placesToMark.Length - currentMarker - 1);
-            Instantiate(marker, placesToMark[currentMarker++].transform.position, Quaternion.identity, transform);
+            else if (currentMarker == placesToMark.Length)
+            {
+                var t = GetComponentInParent<EndCutsceneData>();
+                ShowEndgame.Instance.StartCutscene(t);
+            }
+
+            else
+            {
+                Djzada.Instance.CountNextDay();
+                SetRemainingDays(placesToMark.Length - currentMarker - 1);
+                Instantiate(marker, placesToMark[currentMarker].transform.position, Quaternion.identity, transform);
+            }
+
+            currentMarker++;
         }
-        
+
         void SetRemainingDays(int remainingDays)
         {
             textMeshPro.text = $"{remainingDays} day{(remainingDays == 1 ? "" : "s")} to the execution";
