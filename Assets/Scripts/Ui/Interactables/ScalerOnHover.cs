59ff473ab19b9219689b59fcf180cc946284fc41
theguspassos <theguspassos@gmail.com>
2021-06-19 16:57:46 -0300

    res fix

---
+++ Assets/Scripts/Ui/Interactables/ScalerOnHover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Ui.Interactables
+{
+    public class ScalerOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] float scaleTo;
+        Vector3 originalScale;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            originalScale = transform.localScale;
+            transform.localScale *= scaleTo;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            transform.localScale = originalScale;
+        }
+    }
+}
